649f6aab6d79d4251a2199f25f49db5ebaa6138a
nicokinazoko <nicodunois017157@gmail.com>
2020-12-09 19:02:35 +0700

    Initial commit

---
+++ src/transaction_db4o/Transaction_db4o.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transaction_db4o;
+
+/**
+ *
+ * @author TUF
+ */
+public class Transaction_db4o {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
